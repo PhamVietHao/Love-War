2e0bde683c326aef1d79f690fcaefea397d9f9a2
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2023-08-09 12:40:22 +0700

    Initial Commit

---
+++ Love&War/Love_WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_WarApp.swift
+//  Love&War
+//
+//  Created by mac on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Love_WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
